f8462bff1c919ac87065149c51bfa4d7685740cf
loicburdet <49837621+loicburdet@users.noreply.github.com>
2019-05-09 09:47:43 +0200

    Update module.js

--- Sample.module/module.js
+++ Sample.module/module.js
@@ -1,1 +1,7 @@
 // js
+$('button.open_bot').on('click', (e) => {
+  e.preventDefault();
+  if (window.hubspot_live_messages_running && window.hubspot && window.hubspot.messages) {
+    window.hubspot.messages.EXPERIMENTAL_API.requestWidgetOpen();
+  }
+});
