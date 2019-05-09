5016331e2172c5dae14368ccd3dc18f660d9813e
loicburdet <49837621+loicburdet@users.noreply.github.com>
2019-05-09 09:52:27 +0200

    Update module.js

--- Sample.module/module.js
+++ Sample.module/module.js
@@ -1,5 +1,4 @@
-// js
-$('button.open_bot').on('click', (e) => {
+$('a.open_bot').on('click', (e) => {
   e.preventDefault();
   if (window.hubspot_live_messages_running && window.hubspot && window.hubspot.messages) {
     window.hubspot.messages.EXPERIMENTAL_API.requestWidgetOpen();
